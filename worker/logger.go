17894e957909971bb9a0a995bc3f5ff1db290447
bonus <paramas.wea.th@gmail.com>
2026-08-25 16:18:12 +0700

    ✨ feat:

---
+++ worker/logger.go
@@ -0,0 +1,39 @@
+package worker
+
+import (
+	"fmt"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+type Logger struct {
+}
+
+func NewLogger() *Logger {
+	return &Logger{}
+}
+
+func (logger *Logger) Print(level zerolog.Level, args ...any) {
+	log.WithLevel(level).Msg(fmt.Sprint(args...))
+}
+
+func (logger *Logger) Debug(args ...any) {
+	logger.Print(zerolog.DebugLevel, args...)
+}
+
+func (logger *Logger) Info(args ...any) {
+	logger.Print(zerolog.InfoLevel, args...)
+}
+
+func (logger *Logger) Warn(args ...any) {
+	logger.Print(zerolog.WarnLevel, args...)
+}
+
+func (logger *Logger) Error(args ...any) {
+	logger.Print(zerolog.ErrorLevel, args...)
+}
+
+func (logger *Logger) Fatal(args ...any) {
+	logger.Print(zerolog.FatalLevel, args...)
+}
